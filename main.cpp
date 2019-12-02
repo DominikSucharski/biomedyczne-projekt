6ec4b32fd19b6d1526eb4339bc9aeb599c883953
DominikSucharski <>
2019-12-02 22:21:08 +0100

    Pomiar odległości między oczami w px

--- main.cpp
+++ main.cpp
@@ -3,16 +3,15 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 using namespace cv;
 
-void detectAndDraw(Mat& img, CascadeClassifier& cascade,
-  CascadeClassifier& nestedCascade,
-  double scale, bool tryflip);
+
+void detectAndDraw(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale, bool tryflip);
 string cascadeName;
 string nestedCascadeName;
-
 
 
 int main(int argc, const char** argv)
@@ -56,7 +55,8 @@ int main(int argc, const char** argv)
   
   if (!capture.isOpened())  // sprawdzenie czy kamera jest otwarta
   {
-    capture = VideoCapture(0); // otwarcie domyœlnej kamery
+    // otwarcie domyœlnej kamery, je¿eli kamera o id id_kamery jest niedostêpna
+    capture = VideoCapture(0);
   }
   if (!capture.isOpened())
     return -1;
@@ -82,10 +82,7 @@ int main(int argc, const char** argv)
 
 
 
-void detectAndDraw(Mat& img, CascadeClassifier& cascade,
-  CascadeClassifier& nestedCascade,
-  double scale, bool tryflip)
-{
+void detectAndDraw(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale, bool tryflip) {
   double t = 0;
   vector<Rect> faces, faces2;
   const static Scalar colors[] =
@@ -126,7 +123,7 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade,
     }
   }
   t = (double)getTickCount() - t;
-  printf("detection time = %g ms\n", t * 1000 / getTickFrequency());
+  // printf("detection time = %g ms\n", t * 1000 / getTickFrequency());
   for (size_t i = 0; i < faces.size(); i++)
   {
     Rect r = faces[i];
@@ -136,11 +133,13 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade,
     Scalar color = colors[i % 8];
     int radius;
     double aspect_ratio = (double)r.width / r.height;
+
+    // rysowanie okrêgów wokó³ twarzy
     if (0.75 < aspect_ratio && aspect_ratio < 1.3)
     {
       center.x = cvRound((r.x + r.width*0.5)*scale);
       center.y = cvRound((r.y + r.height*0.5)*scale);
-      radius = cvRound((r.width + r.height)*0.25*scale);
+      radius = cvRound((r.width + r.height)*0.25*scale);  // wielkoœæ okrêgów detekcji twarzy 0.25
       circle(img, center, radius, color, 3, 8, 0);
     }
     else
@@ -157,13 +156,29 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade,
       //|CASCADE_DO_CANNY_PRUNING
       | CASCADE_SCALE_IMAGE,
       Size(30, 30));
+
+    Point oko1, oko2;  // œrodek oka 1 i 2
+
+    // rysowanie okrêgów wokó³ oczu
     for (size_t j = 0; j < nestedObjects.size(); j++)
     {
       Rect nr = nestedObjects[j];
       center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale);
       center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale);
-      radius = cvRound((nr.width + nr.height)*0.25*scale);
+      if (j == 0) oko1 = Point(center.x, center.y);
+      else if(j == 1) oko2 = Point(center.x, center.y);
+      radius = cvRound((nr.width + nr.height)*0.25*scale);  // wielkoœæ okrêgów detekcji oczu 0.25
       circle(img, center, radius, color, 3, 8, 0);
+    }
+
+    if(nestedObjects.size() < 1) putText(img, "Nie wykryto oczu", Point(50, 100), cv::FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+    else if(nestedObjects.size() < 2) putText(img, "Wykryto jedno oko", Point(50, 100), cv::FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+    else {
+      int diff_x = abs(oko1.x - oko2.x);
+      stringstream temp;
+      temp << diff_x;
+      putText(img, "Odleglosc miedzy oczami: " + temp.str() + "px", Point(50, 100), cv::FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0));
+      cout << "Odleglosc miedzy oczami: " + temp.str() + "px" << endl;
     }
   }
   imshow("result", img);
