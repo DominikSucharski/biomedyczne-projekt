4dc13c57a20cb5d56a3d02bc5f3e3b0a7b393203
Navvrot <37208772+Navvrot@users.noreply.github.com>
2019-12-04 01:30:28 +0100

    poprawa komentarzy

--- pomiar_karty.cpp
+++ pomiar_karty.cpp
@@ -23,14 +23,14 @@ double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0) {
 
 int PomiarKarty::WykonajPomiar(Mat& image, vector<Point>& approx)
 {
-	// blur will enhance edge detection
+	// poprawienie detekcji krawedzi
 	Mat blurred(image);
 	medianBlur(image, blurred, 9);
 
 	Mat gray0(blurred.size(), CV_8U), gray;
 	vector<vector<Point> > contours;
 
-	// find squares in every color plane of the image
+	//znalezienie kartki czesciowo przyslonietej
 	for (int c = 0; c < 3; c++)
 	{
 		int ch[] = { c, 0 };
@@ -40,13 +40,12 @@ int PomiarKarty::WykonajPomiar(Mat& image, vector<Point>& approx)
 		const int threshold_level = 2;
 		for (int l = 0; l < threshold_level; l++)
 		{
-			// Use Canny instead of zero threshold level!
-			// Canny helps to catch squares with gradient shading
+			//wylapanie krawedzi przy roznych odcieniach kartki
 			if (l == 0)
 			{
 				Canny(gray0, gray, 10, 20, 3); // 
 
-				// Dilate helps to remove potential holes between edge segments
+				// usuniete mozliwych "dziur" pomiedzy segmentami obrazu
 				dilate(gray, gray, Mat(), Point(-1, -1));
 			}
 			else
@@ -54,30 +53,23 @@ int PomiarKarty::WykonajPomiar(Mat& image, vector<Point>& approx)
 				gray = gray0 >= (l + 1) * 255 / threshold_level;
 			}
 
-			// Find contours and store them in a list
+			//znalezienie konturow kartki i zapis do listy
 			findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
 			
-			// Test contours
+			// kontury
 			for (size_t i = 0; i < contours.size(); i++)
 			{
-				// approximate contour with accuracy proportional
-				// to the contour perimeter
+				
 				approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
 
-				// Note: absolute value of an area is used because
-				// area may be positive or negative - in accordance with the
-				// contour orientation
 				if (approx.size() == 4 && fabs(contourArea(Mat(approx))) > 1000 && isContourConvex(Mat(approx))) 
 				{
-			
 					double maxCosine = 0;
-
 					for (int j = 2; j < 5; j++)
 					{
 						double cosine = fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
 						maxCosine = MAX(maxCosine, cosine);
 					}
-
 					if (maxCosine < 0.3) {
 						int v3 = abs(approx[0].x - approx[2].x) + abs(approx[0].y - approx[2].y);
 						int v2 = abs(approx[0].x - approx[1].x) + abs(approx[0].y - approx[1].y);
